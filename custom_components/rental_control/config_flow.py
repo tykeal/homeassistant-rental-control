8dcabb0b1e98a45131d0adf48d35972ff0b2ad7c
Andrew Grimberg <tykeal@bardicgrove.org>
2021-08-07 13:47:22 -0700

    Refactor: Rebuild _get_schema in prep for options
    Doing the options flow means that we need to handle basically the same
    configuration for a new setup as for an existing one. Taking inspiration
    from the flow handling in keymaster this is the first step in
    refactoring to make this possible.
    
    Signed-off-by: Andrew Grimberg <tykeal@bardicgrove.org>

--- custom_components/rental_control/config_flow.py
+++ custom_components/rental_control/config_flow.py
@@ -1,9 +1,9 @@
 """Config flow for Rental Control integration."""
-from __future__ import annotations
-
 import logging
 import re
 from typing import Any
+from typing import Dict
+from typing import Optional
 
 import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
@@ -12,6 +12,7 @@ from homeassistant import exceptions
 from homeassistant.const import CONF_NAME
 from homeassistant.const import CONF_URL
 from homeassistant.const import CONF_VERIFY_SSL
+from voluptuous.schema_builder import ALLOW_EXTRA
 
 from .const import CONF_CHECKIN
 from .const import CONF_CHECKOUT
@@ -23,48 +24,23 @@ from .const import DEFAULT_DAYS
 from .const import DEFAULT_MAX_EVENTS
 from .const import DOMAIN
 
+# from typing import Union
+
 _LOGGER = logging.getLogger(__name__)
-
-
-def _get_config_schema(input_dict: dict[str, Any] = None) -> vol.Schema:
-    """
-    Return schema defaults for init step based on user input/config dict.
-
-    Retain info already provided for future form views by setting them as
-    defaults in schema.
-    """
-    if input_dict is None:
-        input_dict = {}
-
-    return vol.Schema(
-        {
-            vol.Required(CONF_NAME, default=input_dict.get(CONF_NAME)): cv.string,
-            vol.Required(CONF_URL, default=input_dict.get(CONF_URL)): cv.string,
-            vol.Required(
-                CONF_CHECKIN, default=input_dict.get(CONF_CHECKIN, DEFAULT_CHECKIN)
-            ): cv.string,
-            vol.Required(
-                CONF_CHECKOUT, default=input_dict.get(CONF_CHECKOUT, DEFAULT_CHECKOUT)
-            ): cv.string,
-            vol.Optional(
-                CONF_DAYS, default=input_dict.get(CONF_DAYS, DEFAULT_DAYS)
-            ): cv.positive_int,
-            vol.Optional(
-                CONF_MAX_EVENTS,
-                default=input_dict.get(CONF_MAX_EVENTS, DEFAULT_MAX_EVENTS),
-            ): cv.positive_int,
-            vol.Optional(
-                CONF_VERIFY_SSL, default=input_dict.get(CONF_VERIFY_SSL, True)
-            ): cv.boolean,
-        },
-        extra=vol.REMOVE_EXTRA,
-    )
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Rental Control."""
 
     VERSION = 1
+
+    DEFAULTS = {
+        CONF_CHECKIN: DEFAULT_CHECKIN,
+        CONF_CHECKOUT: DEFAULT_CHECKOUT,
+        CONF_DAYS: DEFAULT_DAYS,
+        CONF_MAX_EVENTS: DEFAULT_MAX_EVENTS,
+        CONF_VERIFY_SSL: True,
+    }
 
     def __init__(self) -> None:
         """Initialize config flow."""
@@ -75,7 +51,8 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         errors = {}
         if user_input is not None:
             # Store current values in case setup fails and user needs to edit
-            self._user_schema = _get_config_schema(user_input)
+            # self._user_schema = _get_config_schema(user_input)
+            self._user_schema = _get_schema(user_input, self.DEFAULTS)
 
             # Validate user input
             try:
@@ -99,8 +76,48 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     title=user_input[CONF_NAME], data=user_input
                 )
 
-        schema = self._user_schema or _get_config_schema()
+        # schema = self._user_schema or _get_config_schema()
+        schema = self._user_schema or _get_schema(user_input, self.DEFAULTS)
         return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
+
+
+def _get_schema(
+    # hass: HomeAssistant,
+    user_input: Optional[Dict[str, Any]],
+    default_dict: Dict[str, Any],
+    # entry_id: str = None,
+) -> vol.Schema:
+    """Gets a schema using the default_dict as a backup."""
+    if user_input is None:
+        user_input = {}
+
+    def _get_default(key: str, fallback_default: Any = None) -> None:
+        """Gets default value for key."""
+        return user_input.get(key, default_dict.get(key, fallback_default))
+
+    return vol.Schema(
+        {
+            vol.Required(CONF_NAME, default=_get_default(CONF_NAME)): cv.string,
+            vol.Required(CONF_URL, default=_get_default(CONF_URL)): cv.string,
+            vol.Required(
+                CONF_CHECKIN, default=_get_default(CONF_CHECKIN, DEFAULT_CHECKIN)
+            ): cv.string,
+            vol.Required(
+                CONF_CHECKOUT, default=_get_default(CONF_CHECKOUT, DEFAULT_CHECKOUT)
+            ): cv.string,
+            vol.Optional(
+                CONF_DAYS, default=_get_default(CONF_DAYS, DEFAULT_DAYS)
+            ): cv.positive_int,
+            vol.Optional(
+                CONF_MAX_EVENTS,
+                default=_get_default(CONF_MAX_EVENTS, DEFAULT_MAX_EVENTS),
+            ): cv.positive_int,
+            vol.Optional(
+                CONF_VERIFY_SSL, default=_get_default(CONF_VERIFY_SSL, True)
+            ): cv.boolean,
+        },
+        extra=ALLOW_EXTRA,
+    )
 
 
 class BadTime(exceptions.HomeAssistantError):
